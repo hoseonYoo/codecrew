19bca73fcebcaf7b2d8a357d023ccc788aee4ff0
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-05-29 13:15:38 +0900

    카테고리 수정

--- src/main/front/src/layouts/CategoryFilter.js
+++ src/main/front/src/layouts/CategoryFilter.js
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../scss/partials/CategoryFilter.scss";
 import { API_SERVER_HOST } from "../api/memberAPI";
-const prefix = `${API_SERVER_HOST}/api/categories`;
+
+const host = API_SERVER_HOST;
 
 export default function CategoryFilter() {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     axios
-      .get(prefix)
+      .get(`${host}/api/categories`)
       .then((response) => {
-        if (Array.isArray(response.data)) {
-          setCategories(response.data);
-        } else {
-          console.log("Data is not an array!");
-        }
+        console.log(response.data);
+
+        setCategories({ ...response.data });
+        console.log(categories);
       })
       .catch((error) => {
-        console.log("There was an error fetching the categories.");
+        console.log(error);
       });
   }, []);
 
@@ -27,7 +27,12 @@ export default function CategoryFilter() {
       <div className="filterContainer">
         <ul>
           <li className="activeFilter">전체</li>
-          {categories.length > 0 && categories.map((category, index) => <li key={index}>{category}</li>)}
+          {Object.entries(categories).length > 0 &&
+            Object.entries(categories).map(([key, value], index) => (
+              <React.Fragment key={index}>
+                <li>{value}</li>
+              </React.Fragment>
+            ))}
         </ul>
       </div>
     </div>
