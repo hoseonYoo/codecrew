1703dc89730bda622ee4bcbbfd8f60ffb03a7e0c
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-05-29 11:45:45 +0900

    업데이트

--- src/main/front/src/pages/study/AddPage.js
+++ src/main/front/src/pages/study/AddPage.js
@@ -3,9 +3,23 @@ import axios from "axios";
 import BasicLayoutPage from "../../layouts/BasicLayoutPage";
 import "../../scss/pages/AddPage.scss";
 import { API_SERVER_HOST } from "../../api/memberAPI";
+import DaumPostcode from "react-daum-postcode";
 const prefix = `${API_SERVER_HOST}/api/categories`;
 
 const AddPage = () => {
+  //주소입력 모달
+  const [modalState, setModalState] = useState(false);
+  const [inputAddressValue, setInputAddressValue] = useState("");
+
+  const handleAddressSearchClick = () => {
+    setModalState(true); // 모달을 표시하도록 상태 변경
+  };
+
+  const onCompletePost = (data) => {
+    setModalState(false); // 모달을 숨기도록 상태 변경
+    setInputAddressValue(data.address); // 주소 값을 설정
+  };
+
   const [imgSrc, setImgSrc] = useState(null);
 
   const handleFileChange = (e) => {
@@ -33,61 +47,73 @@ const AddPage = () => {
   }, []);
 
   return (
-    <BasicLayoutPage headerTitle="스터디추가">
-      <form>
-        <div className="StudyAddWrap">
-          <div className="StudyAddImg" style={{ backgroundImage: `url(${imgSrc})` }}>
-            <label htmlFor="fileInput">
-              추가
-              <input id="fileInput" type="file" onChange={handleFileChange} />
-            </label>
+    <>
+      {/* 모달창입니다. */}
+      {modalState && (
+        <div style={{ position: "fixed", zIndex: "99999", width: "100vw", height: "100vh", backgroundColor: "rgba(0,0,0,0.5)" }}>
+          <div style={{ width: "100vw", maxWidth: "400px", minWidth: "320px", margin: "0 auto", marginTop: "20vh" }}>
+            <DaumPostcode onComplete={onCompletePost} />
           </div>
-          <div>
-            <h3>스터디명</h3>
-            <input type="text" placeholder="스터디명을 입력해주세요." />
-          </div>
-          <div>
-            <h3>연락처</h3>
-            <input type="text" placeholder="연락처를 입력해주세요." />
-          </div>
-          <div>
-            <h3>주소</h3>
-            <input type="text" placeholder="연락처를 입력해주세요." />
-          </div>
-          <div>
-            <h3>참여일자</h3>
-            <input type="datetime-local" placeholder="연락처를 입력해주세요." />
-          </div>
-          <div>
-            <h3>참여인원</h3>
-            <select>
-              {Array.from({ length: 10 }, (_, index) => (
-                <option key={index} value={index + 1}>
-                  {index + 1}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <h3>카테고리</h3>
-            <select>
-              {categories.map((category, index) => (
-                <option key={index} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <h3>스터디 소개</h3>
-            <textarea placeholder="스터디소개를 입력해주세요."></textarea>
+        </div>
+      )}
+      {/* 모달창입니다. */}
+      <BasicLayoutPage headerTitle="스터디추가">
+        <form>
+          <div className="StudyAddWrap">
+            <div className="StudyAddImg" style={{ backgroundImage: `url(${imgSrc})` }}>
+              <label htmlFor="fileInput">
+                추가
+                <input id="fileInput" type="file" onChange={handleFileChange} />
+              </label>
+            </div>
+            <div>
+              <h3>스터디명</h3>
+              <input type="text" placeholder="스터디명을 입력해주세요." />
+            </div>
+            <div>
+              <h3>연락처</h3>
+              <input type="text" placeholder="연락처를 입력해주세요." />
+            </div>
+            <div>
+              <h3>주소</h3>
+              <input type="text" value={inputAddressValue} placeholder="주소를 입력해주세요." readOnly />
+              <img className="AdressSearch" src="../assets/imgs/icon/ic_serch_gr.svg" alt="searchIcon" onClick={handleAddressSearchClick} />
+            </div>
+            <div>
+              <h3>참여일자</h3>
+              <input type="datetime-local" placeholder="연락처를 입력해주세요." />
+            </div>
+            <div>
+              <h3>참여인원</h3>
+              <select>
+                {Array.from({ length: 10 }, (_, index) => (
+                  <option key={index} value={index + 1}>
+                    {index + 1}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <h3>카테고리</h3>
+              <select>
+                {categories.map((category, index) => (
+                  <option key={index} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <h3>스터디 소개</h3>
+              <textarea placeholder="스터디소개를 입력해주세요."></textarea>
+            </div>
           </div>
           <div className="bottomBtnWrap">
             <button className="btnLargePoint">스터디추가</button>
           </div>
-        </div>
-      </form>
-    </BasicLayoutPage>
+        </form>
+      </BasicLayoutPage>
+    </>
   );
 };
 
