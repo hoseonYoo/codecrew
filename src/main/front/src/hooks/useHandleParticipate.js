1fdd1756af400354110b62a530c3842f6abaf9fc
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-06-05 11:48:15 +0900

    진행사항 업데이트 -> 메인,리드페이지 업데이트

---
+++ src/main/front/src/hooks/useHandleParticipate.js
@@ -0,0 +1,34 @@
+// useHandleParticipate.js
+import { useSelector } from "react-redux";
+import jwtAxios from "../util/jwtUtil";
+import { API_SERVER_HOST } from "../api/studyAPI";
+import useCustomMove from "../hooks/useCustomMove";
+
+const useHandleParticipate = () => {
+  const loginState = useSelector((state) => state.loginSlice);
+  const userEmail = loginState.email;
+  const host = API_SERVER_HOST;
+  // 페이지 이동을 위한 함수들
+  const { moveToLogin } = useCustomMove();
+
+  const handleParticipate = async (studyId) => {
+    if (userEmail) {
+      try {
+        const response = await jwtAxios.post(`${host}/api/study/${studyId}/participate`, {
+          email: userEmail,
+        });
+        console.log(response.data);
+        alert("스터디 참가신청이 완료되었습니다.");
+      } catch (error) {
+        console.error(error);
+        alert("스터디 참가신청이 실패하였습니다.");
+      }
+    } else {
+      moveToLogin();
+    }
+  };
+
+  return handleParticipate;
+};
+
+export default useHandleParticipate;
